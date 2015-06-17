1d9366113e8dd05329725053457c2bc75cf74134
Łukasz Merynda <lmerynda@gmail.com>
2015-06-17 22:11:03 +0200

    zad1 ready

--- gr13/zad1.c
+++ gr13/zad1.c
@@ -11,12 +11,14 @@ int main()
   //dane
   double deltaT;
   double maxT;
+  double fMinusDeltaT;
+  double fZero;
   int n;
+  int counter = 0;
 
   //pomocnicze
-  double f0;
-  double f1;
-  double f2;
+  double t;
+  double f0,f1,f2;//f(x0),f(x1),f(x2) - wartosci funkcji f dla kolejnych krokow (f(0) != fZero)
 
   printf("Podaj delta t\n");
   scanf("%lf",&deltaT);
@@ -24,8 +26,27 @@ int main()
   printf("Podaj max t\n");
   scanf("%lf",&maxT);
 
+  printf("Zdefiniuj f(-delta t)\n");
+  scanf("%lf",&fMinusDeltaT);
+
+  printf("Zdefiniuj f(0)\n");
+  scanf("%lf",&fZero);
+
   printf("Podaj n\n");
   scanf("%d",&n);
+
+  f0 = fMinusDeltaT;
+  f1 = fZero;
+
+  for(t=deltaT;t<=maxT;t+=deltaT)
+  {
+    f0 = f1;
+    f1 = f2;
+    f2 = (f1-f0)/(functionG(t-deltaT)+functionG(t-2*deltaT));
+
+    if(++counter % n == 0)
+      printf("Wartosc funkcji f(%lf) wynosi: %lf\n",t,f2);
+  }
 
   return 0;
 }
